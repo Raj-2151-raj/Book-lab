74f122203ce9f73d680221dac046279808bb25ea
raj thakur <rajcthakur07@gmail.com>
2025-08-01 13:12:57 +0530

    modify install redux dependancy

--- src/components/BrowseBooks.jsx
+++ src/components/BrowseBooks.jsx
@@ -38,7 +38,7 @@ const BrowseBooks = () => {
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
           placeholder="Search books..."
-          className="px-4 py-2 w-104 rounded-md border border-gray-300 shadow-sm  focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="bg-gray-50 px-4 py-2 w-104 rounded-md border border-gray-300 shadow-sm  focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
         <button
           onClick={handleSearch}
